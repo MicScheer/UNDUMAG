6218577c28877bf88137f8bce4fca5ddc1b11712
Michael Scheer <Michael.Scheer@Helmholtz-Berlin.de>
2023-09-21 12:54:29 +0200

    CMZ 2.04/20

--- python/undumag_proc.py
+++ python/undumag_proc.py
@@ -14,8 +14,6 @@ t0= time.time()
 
 mPi=np.pi
 
-iSolve = 1
-
 ixsym = 0
 iysym = 0
 izsym = 0
@@ -46,7 +44,70 @@ if izsym!=0:
   rad.TrfZerPara(UnduSetUp,[0,0,0],[0,0,1])
 #endif
 
-#if kDraw!=0,
+#print(rad.ObjGeoLim(AllMagPols[0]))
+
+if kDraw !=0:
+
+  if iSolve == 0 and nUnduNoPolMap == 0:
+
+    FPOLMAP = open("unduradia_pol.map",'w')
+
+    for ipol in range(nUnduPol):
+
+      Voxel = rad.ObjM(UnduPol[ipol])
+      nVoxel = len(Voxel)
+
+      for ivox in range(nVoxel):
+
+        x = Voxel[ivox][0][0]
+        y = Voxel[ivox][0][1]
+        z = Voxel[ivox][0][2]
+
+        FPOLMAP.write(str(x)+ " " + str(y)+ " " + str(z)+ "0 0 0 0  0 0 0 0  0 0 0 0\n ")
+
+      #endfor ivox
+
+    #endfor ipol
+
+    FPOLMAP.close()
+
+  #endif
+
+  if iSolve == 0 and nUnduNoMagMap == 0:
+
+    FMAGMAP = open("unduradia_mag.map",'w')
+
+    for mag in range(nUnduMag):
+
+      Voxel = rad.ObjM(UnduMag[mag])
+      nVoxel = len(Voxel)
+
+      for ivox in range(nVoxel):
+
+        x = Voxel[ivox][0][0]
+        y = Voxel[ivox][0][1]
+        z = Voxel[ivox][0][2]
+
+        FMAGMAP.write(str(x)+ " " + str(y)+ " " + str(z)+ "0 0 0 0  0 0 0 0  0 0 0 0\n ")
+
+      #endfor ivox
+
+    #endfor ipol
+
+    FMAGMAP.close()
+
+  #endif
+
+#  U = rad.ObjM(UnduSetUp)
+#  for iu in range(len(U)):
+#    print(iu,U[iu])
+#  #endfor
+
+#  UnduDmp = rad.UtiDmp(AllMagPols[0], 'asc')
+#
+#  for ud in UnduDmp.split():
+#    print(ud)
+#  #endfor
 #
 #  rad.Plot3DOptions()
 #  UnduDraw = rad.ObjDrw(UnduSetUp)
@@ -57,9 +118,9 @@ if izsym!=0:
 #  (UnduDraw,PlotLabel-> run comment
 #  ,ViewPoint->{100.,100.,100.}],"PDF"]
 #
-#] (* Endif kDraw*)
+#endif
 
-if iSolve!=0:
+if iSolve !=0 :
 
   tiny=0.00
   Prec=0.000001
@@ -121,9 +182,9 @@ if iSolve!=0:
 
       for ivox in range(nVoxel):
 
-        x = Voxel[ivox][0]
-        y = Voxel[ivox][1]
-        z = Voxel[ivox][2]
+        x = Voxel[ivox][0][0]
+        y = Voxel[ivox][0][1]
+        z = Voxel[ivox][0][2]
 
         Field = rad.Fld(UnduSetUp,"bxbybzhxhyhzmxmymz",[x,y,z])
 
@@ -156,16 +217,16 @@ if iSolve!=0:
 
     FMAGMAP = open("unduradia_mag.map",'w')
 
-    for ipol in range(nUnduPol):
+    for ipol in range(nUnduMag):
 
-      Voxel = rad.ObjM(UnduPol[ipol])
+      Voxel = rad.ObjM(UnduMag[ipol])
       nVoxel = len(Voxel)
 
       for ivox in range(nVoxel):
 
-        x = Voxel[ivox][0]
-        y = Voxel[ivox][1]
-        z = Voxel[ivox][2]
+        x = Voxel[ivox][0][0]
+        y = Voxel[ivox][0][1]
+        z = Voxel[ivox][0][2]
 
         Field = rad.Fld(UnduSetUp,"bxbybzhxhyhzmxmymz",[x,y,z])
 
@@ -194,8 +255,8 @@ if iSolve!=0:
 
   #endif nUnduNoMagMap == 0
 
-#  print(" ")
-#  print("BzMaxTot on-axis: ", BzMaxTot)
+  #  print(" ")
+  #  print("BzMaxTot on-axis: ", BzMaxTot)
 
   BzIntInf = rad.FldInt(UnduSetUp, "inf", "bz",[0.,ya,0.], [0.,ye,0.])
   BxIntInf = rad.FldInt(UnduSetUp, "inf", "bx",[0.,ya,0.], [0.,ye,0.])
@@ -204,48 +265,53 @@ if iSolve!=0:
   print("1. vert. Integral on-axis: " ,BzIntInf)
   print("1. hori. Integral on-axis: " ,BxIntInf)
 
+  t1=time.time()
+  print(NL,"SetUp and solution done in:    ",round(t1-t0,2),"  seconds  ")
+
+  if  kForceMagPol > 0:
+
+    Force= rad.FldEnrFrc(AllMagPols(kForceMagPol), UnduSetUp,"fxfyfz")
+
+    Fx=Force[0]
+    Fy=Force[1]
+    Fz=Force[2]
+
+    # Not fully clear, be careful
+
+    if ixsym != 0:
+      Fx=0.
+      Fy=2.*Fy
+      Fz=2.*Fz
+    #endif
+
+    print("Fx, Fy, Fz: ",Fx," ",Fy," ",Fz)
+
+    TCenY = UnduTorqCenX
+    TCenZ = UnduTorqCenY
+    TCenX = UnduTorqCenZ
+
+    TorqueX = rad.FldEnrTrq(AllMagPols[kForceMagPol], UnduSetUp,"tx",[TCenX,TCenY,TCenZ])
+
+    if ixsym!=0:
+      TorqueX=2.*TorqueX
+    #endif
+
+    print("TorqueX: ",TorqueX)
+
+    FILEFORCE = open("unduradia.force",'w')
+    FILEFORCE.write(str(run)+" "+str(Fx)+" "+str(Fy)+" "+str(Fz)+" "+ str(TorqueX)+NL)
+
+    FILEFORCE.close()
+
+    t2=time.time()
+    print("Force calculations done in:    ",round(t2-t1,2),"  seconds  ")
+
+  #endif iUnduForce!=0, *)
+
+else:
+
+  t1=time.time()
+  print(NL,"SetUp done in:    ",round(t1-t0,2),"  seconds  ")
+
 #endif Solve
-
-t1=time.time()
-print(NL,"SetUp and solution done in:    ",round(t1-t0),2,"  seconds  ")
-
-if  kForceMagPol > 0:
-
-  Force= rad.FldEnrFrc(AllMagPols(kForceMagPol), UnduSetUp,"fxfyfz")
-
-  Fx=Force[0]
-  Fy=Force[1]
-  Fz=Force[2]
-
-  # Not fully clear, be careful
-
-  if ixsym != 0:
-     Fx=0.
-     Fy=2.*Fy
-     Fz=2.*Fz
-  #endif
-
-  print("Fx, Fy, Fz: ",Fx," ",Fy," ",Fz)
-
-  TCenY = UnduTorqCenX
-  TCenZ = UnduTorqCenY
-  TCenX = UnduTorqCenZ
-
-  TorqueX = rad.FldEnrTrq(AllMagPols[kForceMagPol], UnduSetUp,"tx",[TCenX,TCenY,TCenZ])
-
-  if ixsym!=0:
-     TorqueX=2.*TorqueX
-  #endif
-
-  print("TorqueX: ",TorqueX)
-
-  FILEFORCE = open("unduradia.force",'w')
-  FILEFORCE.write(str(run)+" "+str(Fx)+" "+str(Fy)+" "+str(Fz)+" "+ str(TorqueX)+NL)
-
-  FILEFORCE.close()
-
-  t2=time.time()
-  print("Force calculations done in:    ",round(t2-t1,2),"  seconds  ")
-
-#endif iUnduForce!=0, *)
 
