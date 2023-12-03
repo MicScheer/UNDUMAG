5c6aed2eec11158a7638d44acf72a29bc5767400
Michael Scheer <Michael.Scheer@Helmholtz-Berlin.de>
2023-12-03 15:41:36 +0100

    3.12.2023

--- for/mod/util_module.f
+++ for/mod/util_module.f
@@ -1,4 +1,5 @@
-*CMZ :          25/09/2023  12.10.25  by  Michael Scheer
+*CMZ :  2.05/03 06/11/2023  12.17.55  by  Michael Scheer
+*CMZ :  2.04/22 25/09/2023  12.10.25  by  Michael Scheer
 *-- Author :    Michael Scheer   25/09/2023
       module utilmod
 
