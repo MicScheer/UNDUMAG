1d2e7098207b68f1991bf96b81a164b88d600fa5
Michael Scheer <Michael.Scheer@Helmholtz-Berlin.de>
2024-12-06 16:32:12 +0100

    Nikolaus 24 b

--- python/undumag_plot.py
+++ python/undumag_plot.py
@@ -32705,7 +32705,7 @@ def read_facets_bounding_box(fb='undumag_bounding_box.fct'):
     Zmax = FcBox[5]
     print('\n--> Done')
   else:
-    wError("File undumag_main_facets not found!")
+    wError("File undumag_main_facets not found! Check, if IUNDUGEO >= 1000 in undumag.nam")
     return
   #endif
 #enddef read_facets_bounding_box(fb='undumag_bounding_box.fct')
@@ -34454,7 +34454,7 @@ def _showGeoUndu(modus='3d',item=-1,kseg=0,callkey=''):
         Facets = [faces,voxels]
         print('\n--> Done')
       else:
-        wError("File undumag_facets.fct not found!")
+        wError("File undumag_facets.fct not found!  Check, if IUNDUGEO >= 1000 in undumag.nam")
         return
       #endif
 
